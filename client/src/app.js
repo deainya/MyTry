0e7ff0208146fba49c80bc04a2f482b34f919e59
deainya <deainya@gmail.com>
2016-10-01 16:40:50 +0400

    Changes is comming...

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,20 @@
 import angular from 'angular'
-angular.module('rfbgo', [])
+import 'angular-ui-router'
+angular.module('rfbgo', ["ui.router"])
 
-.controller('pointsController', function($http){
-  //this.points = ["Test1","Test2"];
-  $http.get('/tradepoints').then((response) => {
-    this.points = response.data;
-  });
+.config(($stateProvider, $urlRouterProvider) => {
+  $urlRouterProvider.otherwise('/tradepoints')
+
+  $stateProvider
+  .state('tradepoints' {
+    url:'/tradepoints',
+    templateUrl:'tradepoints/tradepoints-nav.html',
+    controller: function ($http){
+      //this.points = ["Test1","Test2"];
+      $http.get('/tradepoints').then((response) => {
+        this.points = response.data;
+      });
+    },
+    controllerAs: 'pointsCtrl'
+  })
 })
