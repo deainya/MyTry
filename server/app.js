671b7368dfaeb7e8f98c30ca81aa93a480fff8b5
deainya <deainya@gmail.com>
2016-10-01 13:38:55 +0400

    Adding mongo

--- server/app.js
+++ server/app.js
@@ -3,10 +3,22 @@
 let express = require( "express" );
 let app = express();
 
+let mongoUtil = require('./mongoUtil');
+mongoUtil.connect();
+
 app.use( express.static(__dirname + "/../client") );
 
 app.get("/tradepoints", (request, response) => {
-  response.json( ["Test3","Test4"] );
+  //response.json( ["Test3","Test4"] );
+  let points = mongoUtil.tradepoints(); //Assigning tradepoints collection
+  points.find().toArray((err,docs) => { //making ~select request
+    if(err) {
+      response.sendStatus(400);
+    }
+    console.log(JSON.stringify(docs)); //loging to CON
+    let pointNames = docs.map((point) => point.name);
+    response.json( pointNames );
+  });
 });
 
 app.listen(3001, () => console.log( "Listening on 3001" ) );
