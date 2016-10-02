cd8a8e487407d556f2cf89f1e7a55823db2173a3
deainya <deainya@gmail.com>
2016-10-02 13:01:40 +0400

    Crazy

--- server/app.js
+++ server/app.js
@@ -3,8 +3,33 @@
 let express = require( "express" );
 let app = express();
 
+let configDB = require('./config/database.js');
+let mongoose = require('mongoose');
 let mongoUtil = require('./mongoUtil');
+let passport = require('passport');
+let flash    = require('connect-flash');
+
 mongoUtil.connect();
+mongoose.connect(configDB.url); // connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
+
+app.configure(function() {
+	// set up our express application
+	app.use(express.logger('dev')); // log every request to the console
+	app.use(express.cookieParser()); // read cookies (needed for auth)
+	app.use(express.bodyParser()); // get information from html forms
+
+	app.set('view engine', 'ejs'); // set up ejs for templating
+
+	// required for passport
+	app.use(express.session({ secret: 'thisisapartnergoappbyrfb' })); // session secret
+	app.use(passport.initialize());
+	app.use(passport.session()); // persistent login sessions
+	app.use(flash()); // use connect-flash for flash messages stored in session
+});
+
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 app.use( express.static(__dirname + "/../client") );
 
